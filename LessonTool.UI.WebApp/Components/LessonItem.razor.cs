c0d476cc5d948da6aefcf531f2270ec3bf67117b
DavidBaillie <DavidlBaillie@gmail.com>
2024-02-11 12:31:29 -0500

    Starting work on web

---
+++ LessonTool.UI.WebApp/Components/LessonItem.razor.cs
@@ -0,0 +1,13 @@
+﻿using LessonTool.Common.Domain.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace LessonTool.UI.WebApp.Components;
+
+public partial class LessonItem
+{
+    [Inject]
+    private LessonDto lesson { get; set; }
+
+    private string lessonName { get => lesson is null ? "N/A" : lesson.Name; }
+    private string lessonDescription { get => lesson is null ?  "N/A" : lesson.Description; }
+}
