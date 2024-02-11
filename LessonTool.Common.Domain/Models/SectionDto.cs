2ac033e1fa4ebbb863d86c73abcc7f6a28c4de77
DavidBaillie <DavidlBaillie@gmail.com>
2024-02-11 09:49:52 -0500

    Added lessonId support

--- LessonTool.Common.Domain/Models/SectionDto.cs
+++ LessonTool.Common.Domain/Models/SectionDto.cs
@@ -2,6 +2,7 @@
 
 public class SectionDto : EntityDtoBase
 {
+    public Guid LessonId { get; set; }
     public string Title { get; set; }
     public string Content { get; set; }
     public DateTime CreatedDate { get; set; }
