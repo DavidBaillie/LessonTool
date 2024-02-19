eabbc4942ab8a1db1fc1cfe422e592fe124fe7e7
DavidBaillie <DavidlBaillie@gmail.com>
2024-02-19 10:49:30 -0500

    Subdividing repo interfaces

---
+++ LessonTool.Common.Domain/Interfaces/IReadOnlyInterface.cs
@@ -0,0 +1,11 @@
+﻿namespace LessonTool.Common.Domain.Interfaces;
+
+public interface IReadOnlyInterface<T>
+{
+    /// <summary>
+    /// Gets a single T from the repository by Id
+    /// </summary>
+    /// <param name="id">Id of entity</param>
+    /// <param name="cancellationToken">Process token</param>
+    Task<T> GetAsync(Guid id, CancellationToken cancellationToken = default);
+}
---
+++ LessonTool.Common.Domain/Interfaces/IWriteOnlyRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace LessonTool.Common.Domain.Interfaces;
+
+public interface IWriteOnlyRepository<T>
+{
+    /// <summary>
+    /// Creates a new T in the repository
+    /// </summary>
+    /// <param name="entity">Entity to create</param>
+    /// <param name="cancellationToken">Process token</param>
+    Task<T> CreateAsync(T entity, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Updates an existing T in the repository
+    /// </summary>
+    /// <param name="entity">Entity to update</param>
+    /// <param name="cancellationToken">Process token</param>
+    Task<T> UpdateAsync(T entity, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Deletes an existing entity by Id
+    /// </summary>
+    /// <param name="id">Id of entity to delete</param>
+    /// <param name="cancellationToken">Process token</param>
+    Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+}
