e24c1970805fe62ba93da377cce112bec8c024d2
DavidBaillie <DavidlBaillie@gmail.com>
2024-02-25 11:10:33 -0500

    Added edit page and navigation

---
+++ LessonTool.UI.WebApp/Pages/LessonsPage.razor.cs
@@ -0,0 +1,10 @@
+﻿using LessonTool.Common.Domain.Interfaces;
+using Microsoft.AspNetCore.Components;
+
+namespace LessonTool.UI.WebApp.Pages;
+
+public partial class LessonsPage
+{
+    [Inject]
+    private ILessonRepository lessonRepository { get; set; }
+}
