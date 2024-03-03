5541a40dfc2e968ee46859f214882af23c17ea0c
DavidBaillie <DavidlBaillie@gmail.com>
2024-03-03 17:14:37 -0500

    Swapping for Identity provider

---
+++ LessonTool.API.Infrastructure/Models/CosmosIdentityUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace LessonTool.API.Infrastructure.Models;
+
+public class CosmosIdentityUser : IdentityUser
+{
+
+}
---
+++ LessonTool.API.Infrastructure/Repositories/IdentityUserRepository.cs
@@ -0,0 +1,164 @@
+﻿using LessonTool.API.Infrastructure.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace LessonTool.API.Infrastructure.Repositories;
+
+public sealed class IdentityUserRepository : IUserStore<CosmosIdentityUser>, IUserClaimStore<CosmosIdentityUser>, IUserRoleStore<CosmosIdentityUser>,
+    IUserPasswordStore<CosmosIdentityUser>, IUserLockoutStore<CosmosIdentityUser>
+{
+    public Task AddClaimsAsync(CosmosIdentityUser user, IEnumerable<Claim> claims, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddToRoleAsync(CosmosIdentityUser user, string roleName, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IdentityResult> CreateAsync(CosmosIdentityUser user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IdentityResult> DeleteAsync(CosmosIdentityUser user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<CosmosIdentityUser> FindByIdAsync(string userId, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<CosmosIdentityUser> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> GetAccessFailedCountAsync(CosmosIdentityUser user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IList<Claim>> GetClaimsAsync(CosmosIdentityUser user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> GetLockoutEnabledAsync(CosmosIdentityUser user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<DateTimeOffset?> GetLockoutEndDateAsync(CosmosIdentityUser user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> GetNormalizedUserNameAsync(CosmosIdentityUser user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> GetPasswordHashAsync(CosmosIdentityUser user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IList<string>> GetRolesAsync(CosmosIdentityUser user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> GetUserIdAsync(CosmosIdentityUser user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> GetUserNameAsync(CosmosIdentityUser user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IList<CosmosIdentityUser>> GetUsersForClaimAsync(Claim claim, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IList<CosmosIdentityUser>> GetUsersInRoleAsync(string roleName, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> HasPasswordAsync(CosmosIdentityUser user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> IncrementAccessFailedCountAsync(CosmosIdentityUser user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> IsInRoleAsync(CosmosIdentityUser user, string roleName, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveClaimsAsync(CosmosIdentityUser user, IEnumerable<Claim> claims, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveFromRoleAsync(CosmosIdentityUser user, string roleName, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task ReplaceClaimAsync(CosmosIdentityUser user, Claim claim, Claim newClaim, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task ResetAccessFailedCountAsync(CosmosIdentityUser user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SetLockoutEnabledAsync(CosmosIdentityUser user, bool enabled, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SetLockoutEndDateAsync(CosmosIdentityUser user, DateTimeOffset? lockoutEnd, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SetNormalizedUserNameAsync(CosmosIdentityUser user, string normalizedName, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SetPasswordHashAsync(CosmosIdentityUser user, string passwordHash, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SetUserNameAsync(CosmosIdentityUser user, string userName, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IdentityResult> UpdateAsync(CosmosIdentityUser user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
