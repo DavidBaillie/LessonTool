03278b02b7167b9f1ddcde0763c0d8de345c34ce
DavidBaillie <DavidlBaillie@gmail.com>
2024-03-03 14:19:18 -0500

    Adding common business layer

---
+++ LessonTool.Common.Application/Interfaces/IStringHashService.cs
@@ -0,0 +1,7 @@
+﻿namespace LessonTool.Common.Application.Interfaces
+{
+    public interface IStringHashService
+    {
+        string HashPassword(string password, string? salt = null);
+    }
+}
---
+++ LessonTool.Common.Application/Services/PasswordHashingService.cs
@@ -0,0 +1,11 @@
+﻿using LessonTool.Common.Application.Interfaces;
+
+namespace LessonTool.Common.Application.Services;
+
+public class PasswordHashingService : IStringHashService
+{
+    public string HashPassword(string password, string? salt = null)
+    {
+        return string.Empty;
+    }
+}
