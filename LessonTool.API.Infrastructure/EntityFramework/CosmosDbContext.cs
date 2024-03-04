f8382e7a67d197e3332332a906a5a79af0639564
DavidBaillie <DavidlBaillie@gmail.com>
2024-03-04 17:16:11 -0500

    Adding EF (#3)

---
+++ LessonTool.API.Infrastructure/EntityFramework/CosmosDbContext.cs
@@ -0,0 +1,35 @@
+﻿using LessonTool.API.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace LessonTool.API.Infrastructure.EntityFramework;
+
+public class CosmosDbContext(IConfiguration configuration) : DbContext
+{
+    public DbSet<CosmosLesson> Lessons { get; set; }
+    public DbSet<CosmosSection> Sections { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseCosmos(
+            configuration.GetSection("CosmosOptions")["Endpoint"],
+            configuration.GetSection("CosmosOptions")["AccountKey"],
+            configuration.GetSection("CosmosOptions")["DatabaseName"]);
+
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<CosmosLesson>()
+            .ToContainer("Data")
+            .HasPartitionKey("Type");
+
+        modelBuilder.Entity<CosmosSection>()
+            .ToContainer("Data")
+            .HasPartitionKey("Type");
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
