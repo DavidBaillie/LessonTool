083b558e05fb894bd97d82d67814d9e23d26ac48
DavidBaillie <DavidlBaillie@gmail.com>
2024-04-28 16:07:59 -0400

    Building user management

---
+++ LessonTool.API.Endpoint/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using LessonTool.API.Authentication.Constants;
+using LessonTool.API.Infrastructure.Models;
+using LessonTool.Common.Domain.Interfaces;
+using LessonTool.Common.Domain.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LessonTool.API.Endpoint.Controllers
+{
+    [ApiController]
+    [Authorize(Policy = PolicyNameConstants.AdminPolicy)]
+    [Route("/users")]
+    public class UsersController(IRepository<CosmosUserAccount> userRepository) 
+        : ControllerBase
+    {
+        //[HttpGet]
+        //public async Task<ActionResult<UserDto>> GetUsers(CancellationToken cancellationToken)
+        //{
+            
+        //}
+    }
+}
---
+++ LessonTool.UI.Infrastructure/HttpServices/UsersEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using LessonTool.UI.Infrastructure.Constants;
+using LessonTool.UI.Infrastructure.Interfaces;
+using System.Text.Json;
+
+namespace LessonTool.UI.Infrastructure.HttpServices;
+
+public class UsersEndpoint(IAuthenticationStateHandler _authenticationStateHandler, IHttpClientFactory _clientFactory)
+{
+    private JsonSerializerOptions jsonOptions { get; set; } = new(JsonSerializerDefaults.Web);
+
+    private HttpClient GetHttpClient() => _clientFactory.CreateClient(ApiEndpointConstants.CommonApiClientName);
+
+
+    
+}
