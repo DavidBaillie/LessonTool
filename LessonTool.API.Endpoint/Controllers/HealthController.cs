9db785456389830b1336e147e8bc6ffb4340d82f
DavidBaillie <DavidlBaillie@gmail.com>
2024-05-10 19:03:03 -0400

    Added health check and routing file

---
+++ LessonTool.API.Endpoint/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LessonTool.API.Endpoint.Controllers;
+
+[ApiController]
+[Route("/api/polling/alive")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult Get()
+    {
+        return Ok("Api is alive");
+    }
+}
